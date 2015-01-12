d67b1222e683db23f9e3b4afd86d92d47d91c5b8
Nate <natanel7@gmail.com>
2015-01-12 17:56:20 +0200

    1. var declarations moved to top 2. Unless a new list should be created, use an existing list.

--- js/app.js
+++ js/app.js
@@ -3,13 +3,15 @@ $(function () {
 	////////////////
 	// Variables //
 	////////////////
-	var $form    = $("#contact-form"),
-		$fields  = $form.find("input, textarea, select"),
-		$select  = $form.find("select#group-assign"),
-		$chkBox  = $form.find("input#is-work-contact"),
-		$modal   = $("#contact-modal"),
-		orgTitle = $modal.find("#modal-title").html(),
-		promise  = $.getJSON('data/MOCK_DATA_REGULAR.json'),
+	var $container 	  = $("#contacts"),
+		$form 	      = $("#contact-form"),
+		$fields       = $form.find("input, textarea, select"),
+		$select       = $form.find("select#group-assign"),
+		$chkBox       = $form.find("input#is-work-contact"),
+		$modal 	      = $("#contact-modal"),
+        contactId 	  = myBook.getContactId(),
+		orgTitle      = $modal.find("#modal-title").html(),
+		promise       = $.getJSON('data/MOCK_DATA_REGULAR.json'),
 		isWorkContact,
 		domElWork = '\
 				<input type="text" name="position" id="position" class="form-control input-lg" placeholder="Position" tabindex="12">\
@@ -31,10 +33,14 @@ $(function () {
 		$input9  = $form.find("#facebook-page").val("http://facebook.com");
 		$input10 = $form.find("#comments").val("For performance reasons, all icons require a base class and individual icon class. To use, place the following code just about anywhere. Be sure to leave a space between the icon and text for proper padding.");
 
+	// Update the DOM with the current Contact id
+	$('input#id').val((contactId + 1));
+
 	//////////////////////
 	// init checkboxes //
 	//////////////////////
 	function addCheckboxes(){
+        
 		$('.contact-item').each(function () {
 			var $widget = $(this),
 				$input = $widget.find('input'),
@@ -53,7 +59,7 @@ $(function () {
 			if($widget.find("span.state-icon").length <= 0){ // If checkbox exists, dont create it again.
 
 				$widget.prepend('<span class="pos-icon state-icon ' + settings[type].off.icon + '"  data-toggle="tooltip" title="Click to sort contact"></span>');
-					
+				
 				$widget.find('.state-icon').on('click', function () {
 					$input.prop('checked', !$input.is(':checked'));
 					updateDisplay();
@@ -77,6 +83,7 @@ $(function () {
 		});
 	}
 	addCheckboxes();
+
 	//////////////////////////////////////////////////////////
 	// get items data on click and write it to DOM element //
 	//////////////////////////////////////////////////////////
@@ -93,7 +100,7 @@ $(function () {
 			$('.jumbotron').append('<p id="output-data"></p>');
 		}
 		
-		$("#layout .active").each(function(idx, el) {
+		$("#contacts .active").each(function(idx, el) {
 			checkedItems.idx   = idx;
 			checkedItems.name = $(el).find(".name").text();
 			checkedItems.cid  = $(el).find("a.edit-contact").data('cid');
@@ -108,6 +115,7 @@ $(function () {
 	$('body').tooltip({
 		selector: '[data-toggle="tooltip"], [data-tooltip="true"]'
 	});
+
 	//////////////////////////////////
 	// init jquery UI - DatePicker //
 	//////////////////////////////////
@@ -168,28 +176,47 @@ $(function () {
 		e.preventDefault();
 	});
 
+	/////////////////////////////
+	// build the lists select //
+	/////////////////////////////
+	buildContactsListsSelect($select, myBook);
+
 	//////////////////
 	// Form submit //
 	//////////////////
 	$form.on('submit', function(e) {
 
-		var data = $form.serializeArray(),
+		var
+			data   = $form.serializeArray(),
+			len    = data.length,
+			tmpObj = {},
+			$row   = $("div[data-role='wrapper']"),
 			i,
-			len = data.length,
 			key,
 			value,
 			list,
+			id,
+			newId,
 			newContact,
 			newList,
-			tmpObj = {};		
+			contact,
+			item;
 		
-		// iterate through the serializeArray Objects
-		// and create an accessible new 'tmpObj'
+		/**
+		 * iterate through the serializeArray Objects
+		 * and create an accessible new 'tmpObj'
+		 */
 		$.each(data, function(index, val) {
 			 key = val["name"];
-			 if(key === "groupAssign"){
-			 	tmpObj[key]        = $select.find("option:selected").text();
+			 //  pause on 'groupAssign' field and get the text value of the <option>,
+			 //  the value of the <option> is used for the cssClass property.
+			 if(key === "groupAssign"){ 
+			 	tmpObj[key]        = $select.find(":selected").text();
 			 	tmpObj["cssClass"] = val["value"];
+			 	return;
+			 }
+			 if(key === "id"){ //  pause on 'id' field and convert to integer
+			 	tmpObj[key] = parseInt(val["value"], 10);
 			 	return;
 			 }
 			 tmpObj[key] = val["value"];
@@ -200,117 +227,42 @@ $(function () {
 		if($select.val() == "user" && $("#new-user-list").length > 0){
 			list = $("#new-user-list").val();
 		}
-
+        
 		if (!isWorkContact) {
 			newContact = new ContactsLib.Contact(tmpObj);
 		} else {
 			newContact = new ContactsLib.WorkContact(tmpObj);
 		}
 
-		newList = new ContactsLib.ContactsList(list, [newContact]);
+		if (myBook.get(list)) { // If list already exists
+			newList = myBook.create(list, newContact);
+		} else {
+			id      = myBook.getListId(); // get the cuurrent highest id
+			newId   = (id+1); 	// Advance the current id by 1
+			newList = new ContactsLib.ContactsList(list, [newContact], newId);
+			myBook.add(newList);
+		}
 
-		myBook.add(newList);
-		
 		// Close modal window after submit
 		//$modal.modal('hide');
+		
+		contactElm = createContactWidget(newContact, newList, isWorkContact);
 
-		/////////////////////////////////
-		// Select element interaction //
-		/////////////////////////////////
-		if($select.val() == "user"){
-			var optId = $select.find('option').not("user").last().attr("value");
-			$select.find('option[value="user"]').before('<option value="'+optId+'">'+ $("#new-user-list").val() + '</option>');
-		}
+		$('input#id').val((myBook.getContactId() + 1));  // Update the hidden id input with the highest id + 1
 
-		var contact    = newList.find(tmpObj["id"]),
-			$container = $("#layout"),
-			$row       = $("div[data-role='wrapper']"),
-			newId      = contact.id;
-			item       = '\
-				<div class="col-xs-12 col-sm-4 col-md-4 contact-item-wrapper">\
-					<div data-group="'+contact.cssClass+'" class="thumbnail contact-item relative">\
-						<div class="col-xs-4 col-sm-6 col-md-6">\
-							<img src="'+contact.imageUrl+'" alt="'+contact.firstName+' '+contact.lastName+'" class="img-responsive img-circle" />\
-						</div>\
-						<div class="col-xs-8 col-sm-6 col-md-6">\
-							<h4 class="name">'+contact.firstName+' '+contact.lastName+'</h4>\
-							<span class="glyphicon glyphicon-envelope text-muted c-info" data-toggle="tooltip" title="'+contact.email+'"></span>\
-							<span class="visible-xs"><span class="text-muted">'+contact.email+'</span><br /></span>\
-							<span class="glyphicon glyphicon-phone text-muted c-info" data-toggle="tooltip" title="'+contact.mobile+'"></span>\
-							<span class="visible-xs"><span class="text-muted">'+contact.mobile+'</span><br /></span>\
-							<span class="glyphicon glyphicon-gift text-muted c-info" data-toggle="tooltip" title="'+contact.birthDate+'"></span>\
-							<span class="visible-xs"><span class="text-muted">'+contact.birthDate+'</span><br /></span>\
-							';
-					if(isWorkContact){							
-						item += '\
-							<span class="glyphicon glyphicon-star text-muted c-info" data-toggle="tooltip" title="'+contact.position+'"></span>\
-							<span class="visible-xs"><span class="text-muted">'+contact.position+'</span><br /></span>\
-							<span class="glyphicon glyphicon-star text-muted c-info" data-toggle="tooltip" title="'+contact.color+'"></span>\
-							<span class="visible-xs"><span class="text-muted">'+contact.color+'</span><br /></span>\
-							';
-					}							
-						item += '\
-							<span class="glyphicon glyphicon-earphone text-muted c-info" data-toggle="tooltip" title="'+contact.phone+'"></span>\
-							<span class="visible-xs"><span class="text-muted">'+contact.phone+'</span><br /></span>\
-							<span class="glyphicon glyphicon-briefcase text-muted c-info" data-toggle="tooltip" title="'+contact.workPhone+'"></span>\
-							<span class="visible-xs"><span class="text-muted">'+contact.workPhone+'</span><br /></span>\
-							<a href="'+contact.facebookPage+'"><span class="glyphicon glyphicon-link text-muted c-info" data-toggle="tooltip" title="See my FB page"></span>\
-							<span class="visible-xs"><span class="text-muted break-word">See my FB page</span></span></a>\
-							<br />\
-							<a data-toggle="collapse" href="#collapse-'+newId+'" aria-expanded="false" aria-controls="collapse-'+newId+'"><i class="glyphicon glyphicon-comment text-muted"></i> Show more</a>\
-						</div>\
-						<a href="#" class="pos-icon-top edit-contact" data-cid="'+newId+'" data-tooltip="true" title="Edit contact" data-toggle="modal" data-target="#contact-modal"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>\
-						<input type="checkbox">\
-						<div class="clearfix"></div>\
-						<div class="collapse" id="collapse-'+newId+'">\
-							<div class="well comments">\
-								<span class="text-muted break-word">'+contact.comments+'</span>\
-							</div>\
-						</div>\
-					</div>\
-				</div>';
-
-		newId++; // Advance the id count
-		$('input#id').val(newId);  // Update the hidden id input
-
-		////////////////////////////////////////////////
-		// Append the newly created item to the DOM //
-		////////////////////////////////////////////////
+		/////////////////////////////////////////////////////////
+		// Append the newly created contactElm to the DOM      //
+		/////////////////////////////////////////////////////////
 		if($row.last().length > 0 && $row.last().children().length < 3){ // If row exists on page and items in it does not exceed 3
-			$container.find($row.last()).append(item);
+			$container.find($row.last()).append(contactElm);
 		}else{ // If row doesn't exists on page OR items in row exceed 3
-			$container.find("#layout_footer").before(function() {
-				return '<div class="row" data-role="wrapper">'+item+'</div>';
-			});
+			$container.append('<div class="row" data-role="wrapper">'+contactElm+'</div>');
 		}
+
 		addCheckboxes();
+		buildContactsListsSelect($select, myBook);
 		e.preventDefault();
 	});
-
-	/////////////////
-	// JSON stuff //
-	/////////////////	
-	/*
-	if($("#output-data").length <= 0){
-		$('.jumbotron').append('<p id="output-data"></p>');
-	}
-
-	promise.done(function(data) {
-		//$("#output-data").append(JSON.stringify(data[0], null, '\t'));
-		
-		var autoList = new ContactsLib.ContactsList("Friends2", data);
-		
-		myBook.add(autoList);
-		console.log(myBook.lists);
-
-	});
-	promise.fail(function() {
-		$("#output-data").append('<p>Oh no, something went wrong!</p>');
-	});
-	promise.always(function() {
-		$("#output-data").append('<p>I promise this will always be added regardless if failed or succeeded!.</p>');
-	});
-	*/
 
 /////////////////////
 //-- END DOM READY //
